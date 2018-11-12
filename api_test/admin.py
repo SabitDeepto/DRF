6a52f2b0cb7465fbac1d0658ea7597951ac83fdd
SabitDeepto <deepto69@gmail.com>
2018-11-12 18:01:42 +0600

    jwt

--- api_test/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from api_test.models import Paradigm, Language, Programmer
-
-admin.site.register(Paradigm)
-admin.site.register(Language)
-admin.site.register(Programmer)
