1740aa1e96f2982f5fda5986f668729e8de47e71
SabitDeepto <deepto69@gmail.com>
2018-12-10 16:47:16 +0600

    mysql

--- rest_project/settings.py
+++ rest_project/settings.py
@@ -86,8 +86,12 @@ WSGI_APPLICATION = 'rest_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'tiktok',
+        'USER': 'phpmyadmin',
+        'PASSWORD': 'std123',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
