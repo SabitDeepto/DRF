eb2b1c5830ef2e348c10dea4c0c2acc6762494a1
SabitDeepto <deepto69@gmail.com>
2018-12-10 22:01:59 +0600

    dashboard ui

--- rest_project/urls.py
+++ rest_project/urls.py
@@ -12,13 +12,13 @@ urlpatterns = [
 
     path('jet/', include('jet.urls', 'jet')),
     path('jet/dashboard/', include('jet.dashboard.urls', 'jet-dashboard')),
-    path('admin/', views.admin_view, name='admin'),
+    path('admin/', views.login, name='login'),
+    path('admin/signup', views.signup, name='signup'),
+    path('admin/index', views.index, name='index'),
+
+
 
     path('', include('Manage_Merchants.urls')),
-
-    # path('api-auth', include('rest_framework.urls')),
-    # path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    # path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
 
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)+staticfiles_urlpatterns()
--- rest_project/views.py
+++ rest_project/views.py
@@ -1,20 +1,15 @@
 # from rest_framework import serializers
 from django.shortcuts import render
-# from rest_framework_simplejwt import serializers
-#
-# from rest_project.serializers import TokenObtainPairPatchedSerializer
-# from rest_framework_simplejwt.views import TokenObtainPairView
-#
-#
-# class TokenObtainPairPatchedView(TokenObtainPairView):
-#     """
-#     Takes a set of user credentials and returns an access and refresh JSON web
-#     token pair to prove the authentication of those credentials.
-#     """
-#     serializer_class = serializers.TokenObtainPairPatchedSerializer
-#
-#     token_obtain_pair = TokenObtainPairView.as_view()
 
 
-def admin_view(request):
+def login(request):
     return render(request, 'auth_login.html')
+
+
+def signup(request):
+    return render(request, 'auth_register.html')
+
+
+def index(request):
+    return render(request, 'index.html')
+
