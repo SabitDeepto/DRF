92dde21d7866c9b033693c7dcb518ec534cca7ae
SabitDeepto <deepto69@gmail.com>
2018-12-12 06:46:48 +0600

    configured UI editing

--- rest_project/views.py
+++ rest_project/views.py
@@ -2,10 +2,8 @@
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
-from django.contrib.auth.decorators import login_required
 
 
-@login_required
 def home(request):
     count = User.objects.count()
 
